10043a8c825ba17fb185386b5ad6a0829b646acd
kiryl.zhan <kiryl.zhan@innowise.com>
2026-02-06 00:18:30 +0100

    new text

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     image_name = os.getenv('IMAGE_NAME', 'Unknown (variable not set)')
-    return f"Hello! This container is running image: {image_name}\n"
+    
+    return f"""
+    <html>
+        <body style="background-color: #808080; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; font-family: sans-serif;">
+            <h1 style="font-size: 50px; color: white; text-align: center;">
+                Hello! This container is running image:<br>
+                <span style="color: #f0f0f0;">{image_name}</span>
+            </h1>
+        </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
